0023bdfdbb4bcf766430f1dedad227480152099e
ARTEZON <artezon@yandex.ru>
2024-03-18 09:28:32 +0300

    Реализованы предикаты для нахождения суммы цифр
    1) С помощью рекурсии вверх - digits_sum_up
    2) С помощью рекурсии вниз - digits_sum_down

--- ЛР2/1. Simple recursion predicates.pl
+++ ЛР2/1. Simple recursion predicates.pl
@@ -12,3 +12,13 @@ fact_down(N, X) :- fact_down(0, 1, N, X).
 % fact_down(+N_current, +X_current, +N, -X) - вспомогательный предикат для факториала с рекурсией вниз
 fact_down(N, X, N, X) :- !.
 fact_down(N_current, X_current, N, X) :- N_next is N_current + 1, X_next is X_current * N_next, fact_down(N_next, X_next, N, X).
+
+% digits_sum_up(+N, -Sum) - вычисляет сумму цифр числа N, используя рекурсию вверх
+digits_sum_up(0, 0) :- !.
+digits_sum_up(N, Sum) :- N1 is N // 10, Remainder is N mod 10, digits_sum_up(N1, Sum1), Sum is Sum1 + Remainder.
+
+% digits_sum_down(+N, -Sum) - вычисляет сумму цифр числа N, используя рекурсию вниз
+digits_sum_down(N, Sum) :- digits_sum_down(N, 0, Sum).
+% digits_sum_down(+N, +CurrentSum, -Sum) - вспомогательный предикат для суммы цифр с рекурсией вниз
+digits_sum_down(0, Sum, Sum) :- !.
+digits_sum_down(N, CurrentSum, Sum) :- N1 is N // 10, Remainder is N mod 10, NewSum is CurrentSum + Remainder, digits_sum_down(N1, NewSum, Sum).
