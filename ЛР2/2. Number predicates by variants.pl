00d5820cd79bf9248207ccc8b69be3630858882c
ARTEZON <artezon@yandex.ru>
2024-03-19 06:53:33 +0300

    Созданы предикаты для нахождения минимальной нечётной цифры числа
    Рекурсия вверх, вниз. Вспомогательные предикаты: минимум двух чисел и определение нечётности числа.

--- ЛР2/2. Number predicates by variants.pl
+++ ЛР2/2. Number predicates by variants.pl
@@ -18,3 +18,34 @@ max_digit_down(Number, MaxDigit) :- AbsNumber is abs(Number), max_digit_down(Abs
 max_digit_down(0, CurrentMax, CurrentMax).
 max_digit_down(Number, CurrentMax, MaxDigit) :- CurrentDigit is Number mod 10, NextNumber is Number // 10,
     NewMax is max(CurrentMax, CurrentDigit), max_digit_down(NextNumber, NewMax, MaxDigit).
+
+% Найти минимальную нечётную цифру числа.
+
+% min(+A, +B, -Min) - записывает в Min минимальное число из A и B
+min(A, B, A) :- A < B, !.
+min(_, B, B).
+
+% is_odd(+Number) - определяет, является ли число нечётным
+is_odd(Number) :- Number mod 2 =:= 1.
+
+% min_odd_digit_up(+Number, -MinOddDigit) - находит минимальную нечётную цифру числа с помощью рекурсии вверх
+min_odd_digit_up(0, -1) :- !.
+min_odd_digit_up(Number, MinOddDigit) :- Number < 0 -> AbsNumber is abs(Number), min_odd_digit_up(AbsNumber, MinOddDigit), !.
+min_odd_digit_up(Number, MinOddDigit) :- CurrentDigit is Number mod 10, NextNumber is Number // 10,
+    min_odd_digit_up(NextNumber, NextMinOddDigit), (is_odd(CurrentDigit) -> (NextMinOddDigit =\= -1 ->
+    min(NextMinOddDigit, CurrentDigit, MinOddDigit); MinOddDigit is CurrentDigit); MinOddDigit is NextMinOddDigit).
+
+% min_odd_digit_down(+Number, -MinOddDigit) - находит минимальную нечётную цифру числа с помощью рекурсии вниз
+min_odd_digit_down(Number, MinOddDigit) :- AbsNumber is abs(Number), min_odd_digit_down(AbsNumber, -1, MinOddDigit).
+% min_odd_digit_down(+Number, +CurrentOddMin, -MinOddDigit) - вспомогателный предикат
+min_odd_digit_down(0, CurrentOddMin, CurrentOddMin).
+min_odd_digit_down(Number, CurrentOddMin, MinOddDigit) :- CurrentDigit is Number mod 10, NextNumber is Number // 10,
+    (is_odd(CurrentDigit) -> (CurrentOddMin =\= -1 -> min(CurrentOddMin, CurrentDigit, NewMin);
+    NewMin is CurrentDigit); NewMin is CurrentOddMin), min_odd_digit_down(NextNumber, NewMin, MinOddDigit).
+
+
+
+
+
+
+% Найти НОД двух чисел.
