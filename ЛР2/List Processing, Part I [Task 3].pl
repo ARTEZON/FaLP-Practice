1512d422203918bca6c6cc888fe09fac9d16ee34
ARTEZON <artezon@yandex.ru>
2024-03-30 09:49:04 +0300

    Построены предикаты для нахождения пропущенных чисел

--- ЛР2/List Processing, Part I [Task 3].pl
+++ ЛР2/List Processing, Part I [Task 3].pl
@@ -23,3 +23,29 @@ program__is_min_element :-
     write("Enter "), write(Length), writeln(" numbers separated by dots and spaces:"), read_list(Length, List),
     write("Enter index of your chosen element: "), read(Index),
     (is_min_element(List, Index) -> write("Yes, the element is a global minimum."); write("No, the element is not a global minimum.")).
+
+
+% Дан целочисленный массив. Необходимо найти все пропущенные числа.
+
+% in_list(+List, +Element) - проверяет, находится ли элемент в списке
+in_list([Element|_], Element) :- !.
+in_list([_|T], Element) :- in_list(T, Element).
+
+% max_list_element(+List, -MaxElement) - записывает в MaxElement максимальный элемент списка
+max_list_element([H], H) :- !.
+max_list_element([H|T], MaxElement) :- max_list_element(T, NextMax), max(H, NextMax, MaxElement).
+
+% find_missing_numbers(+List, -Result) - записывает в Result пропущенные целые числа
+find_missing_numbers(List, Result) :- min_list_element(List, Min), max_list_element(List, Max), find_missing_numbers(List, Min, Max, [], Result).
+% find_missing_numbers(+List, +MinNumber, +MaxNumber, +TempList, -Result) - вспомогательный предикат
+find_missing_numbers(_, MaxNumber, MaxNumber, TempList, TempList) :- !.
+find_missing_numbers(List, CurNumber, MaxNumber, TempList, Result) :-
+    (not(in_list(List, CurNumber)) -> append(TempList, [CurNumber], NewTempList); NewTempList = TempList), 
+    NextNumber is CurNumber + 1, find_missing_numbers(List, NextNumber, MaxNumber, NewTempList, Result).
+
+% program__find_missing_numbers/0 - точка входа для нахождения пропущенных чисел
+program__find_missing_numbers :-
+    writeln("The program will find numbers missing from a list."),
+    write("Please enter your list length here: "), read(Length),
+    write("Enter "), write(Length), writeln(" numbers separated by dots and spaces:"), read_list(Length, List),
+    find_missing_numbers(List, Result), write("The missing numbers are "), write_list(Result).
