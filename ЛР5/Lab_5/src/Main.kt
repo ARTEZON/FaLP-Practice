efe78bbef503e3d1c521060e83c1860ccf4f2aa1
ARTEZON <artezon@yandex.ru>
2024-04-26 11:27:40 +0300

    Добавлена функция высшего порядка и вывод названий на экран (ЛР5, задание 1)

--- ЛР5/Lab_5/src/Main.kt
+++ ЛР5/Lab_5/src/Main.kt
@@ -14,11 +14,15 @@ fun digitSumDown(number: Long, curSum: Long = 0): Long {
     return if (number == 0L) newSum else digitSumDown(number / 10, newSum)
 }
 
+fun getFunction(isDigitSum: Boolean): (Long) -> Long = if (isDigitSum) ::digitSumUp else ::factUp
+
 fun main() {
+    println("Максимум")
     println(max(3, 2, 1))
     println(max(-17, -5, -24))
     println(max(-2, 0, 4))
     println()
+    println("Факториал")
     println(factUp(3))
     println(factDown(3))
     println(factUp(10))
@@ -26,9 +30,18 @@ fun main() {
     println(factUp(20))
     println(factDown(20))
     println()
+    println("Сумма цифр")
     println(digitSumUp(1337)) // 14
     println(digitSumDown(1337)) // 14
     println(digitSumUp(654219213)) // 33
     println(digitSumDown(654219213)) // 33
+    println()
+    println("Функция высшего порядка")
+    println(getFunction(false)(1))
+    println(getFunction(false)(3))
+    println(getFunction(false)(5))
+    println(getFunction(true)(5))
+    println(getFunction(true)(10))
+    println(getFunction(true)(15))
 
 }
