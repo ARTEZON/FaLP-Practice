abd92d84ec99a7a57f4de3ba2ad67ce457610d60
ARTEZON <artezon@yandex.ru>
2024-04-26 22:56:18 +0300

    Добавлена функция нахождения мин. нечёт. цифры числа (ЛР5, задание 2)

--- ЛР5/Lab_5/src/2 - Pure Functions with Loops.kt
+++ ЛР5/Lab_5/src/2 - Pure Functions with Loops.kt
@@ -14,10 +14,27 @@ fun maxDigit(n: Int): Int {
     return max
 }
 
+// Найти минимальную нечётную цифру числа.
+fun minOddDigit(n: Int): Int {
+    var min = 9
+    var num = abs(n)
+    while (num > 0) {
+        val d = num % 10
+        if (d < min && d % 2 == 1) min = d
+        num /= 10
+    }
+    return min
+}
+
 fun main() {
     println("Найти максимальную цифру числа")
     println(maxDigit(8192))
     println(maxDigit(0))
     println(maxDigit(-123))
+    println()
+    println("Найти минимальную нечётную цифру числа")
+    println(minOddDigit(238))
+    println(minOddDigit(17))
+    println(minOddDigit(-583))
     println()
 }
