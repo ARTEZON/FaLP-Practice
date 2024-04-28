b9baa6edee33201cb1f119021382af63e4eeee82
ARTEZON <artezon@yandex.ru>
2024-04-29 02:48:59 +0300

    Выделены функции высших порядков, добавлены функции-предикаты (ЛР5, задание 4)

--- ЛР5/Lab_5/src/lab/4 - High Order Functions.kt
+++ ЛР5/Lab_5/src/lab/4 - High Order Functions.kt
@@ -2,33 +2,35 @@ package lab
 
 // Вариант 5
 
-fun invokeFunc(n: Int, f: (Int) -> Int) = f(n)
-fun invokeFunc(n1: Int, n2: Int, f: (Int, Int) -> Int) = f(n1, n2)
+// В качестве функции высшего порядка для нахождения мин. и макс. цифры
+// используется numberTraversal из задания 1.
+
+fun maxDigitRule(a: Int, b: Int) = if (a > b) a else b
+
+fun minOddDigitRule(a: Int, b: Int) = if (b < a && b % 2 == 1) b else a
+
+fun gcdHighOrder(a: Int, b: Int, acceptNewGCDCondition: (Int, Int, Int) -> Boolean, stopCondition: (Int, Int, Int) -> Boolean, newPossibleGCD: Int = 1, curGCD: Int = 1): Int =
+    if (stopCondition(a, b, newPossibleGCD)) curGCD
+    else gcdHighOrder(a, b, acceptNewGCDCondition, stopCondition, newPossibleGCD + 1, if (acceptNewGCDCondition(a, b, newPossibleGCD)) newPossibleGCD else curGCD)
+
+fun gcdStop(a: Int, b: Int, newGCD: Int) = newGCD > a || newGCD > b
+
+fun gcdAccept(a: Int, b: Int, newGCD: Int) =  a % newGCD == 0 && b % newGCD == 0
 
 fun main() {
     println("Найти максимальную цифру числа")
-    println(invokeFunc(8192, ::maxDigitUp))
-    println(invokeFunc(0, ::maxDigitUp))
-    println(invokeFunc(123, ::maxDigitUp))
-    println(invokeFunc(8192, ::maxDigitTail))
-    println(invokeFunc(0, ::maxDigitTail))
-    println(invokeFunc(123, ::maxDigitTail))
+    println(numberTraversal(8192, ::maxDigitRule))
+    println(numberTraversal(0, ::maxDigitRule))
+    println(numberTraversal(123, ::maxDigitRule))
     println()
     println("Найти минимальную нечётную цифру числа")
-    println(invokeFunc(238, ::minOddDigitUp))
-    println(invokeFunc(17, ::minOddDigitUp))
-    println(invokeFunc(583, ::minOddDigitUp))
-    println(invokeFunc(238, ::minOddDigitTail))
-    println(invokeFunc(17, ::minOddDigitTail))
-    println(invokeFunc(583, ::minOddDigitTail))
+    println(numberTraversal(238, ::minOddDigitRule, 9))
+    println(numberTraversal(17, ::minOddDigitRule, 9))
+    println(numberTraversal(583, ::minOddDigitRule, 9))
     println()
     println("Найти НОД двух чисел")
-    println(invokeFunc(3, 5, ::gcdUp))
-    println(invokeFunc(45, 30, ::gcdUp))
-    println(invokeFunc(35, 7, ::gcdUp))
-    println(invokeFunc(36, 60, ::gcdUp))
-    println(invokeFunc(3, 5, ::gcdTail))
-    println(invokeFunc(45, 30, ::gcdTail))
-    println(invokeFunc(35, 7, ::gcdTail))
-    println(invokeFunc(36, 60, ::gcdTail))
+    println(gcdHighOrder(3, 5, ::gcdAccept, ::gcdStop))
+    println(gcdHighOrder(45, 30, ::gcdAccept, ::gcdStop))
+    println(gcdHighOrder(35, 7, ::gcdAccept, ::gcdStop))
+    println(gcdHighOrder(36, 60, ::gcdAccept, ::gcdStop))
 }
