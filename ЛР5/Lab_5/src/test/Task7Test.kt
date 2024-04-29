cd469398519f6a76ced6e86bc886e099fca51330
ARTEZON <artezon@yandex.ru>
2024-04-29 20:59:07 +0300

    Добавлены тесты для функций нахождения делителей (ЛР5, задание 7)

---
+++ ЛР5/Lab_5/src/test/Task7Test.kt
@@ -0,0 +1,21 @@
+package test
+
+import lab.*
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+
+internal class Task7Test {
+
+    @Test
+    fun countDivsNotDivisibleBy3Test() {
+        val expected = mapOf(1 to 1, 3 to 1, 7 to 2, 12 to 3, 16 to 5, 30 to 4, 33 to 2, 50 to 6, 100 to 9)
+        expected.forEach { assertEquals(it.value, countDivs(it.key, {div -> div % 3 != 0})) }
+    }
+
+    @Test
+    fun sumDivsCoprimeWithDigitSumAndNotCoprimeWithDigitProductTest() {
+        val expected = mapOf(1 to 0, 3 to 0, 7 to 0, 12 to 6, 16 to 30, 30 to 17, 33 to 0, 50 to 2, 100 to 216)
+        expected.forEach { assertEquals(it.value, sumDivs(it.key, ::sumDivsPredicate)) }
+    }
+}
