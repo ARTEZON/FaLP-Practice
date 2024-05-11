bb2888f5b5df66a7643325b304d76e13f8afbb9b
ARTEZON <artezon@yandex.ru>
2024-05-11 19:26:51 +0300

    Функции суммы и количества делителей объединены в одну и добавлен аргумент "операция" (ЛР5, задание 7)

--- ЛР5/Lab_5/src/lab/7 - Find Divisors.kt
+++ ЛР5/Lab_5/src/lab/7 - Find Divisors.kt
@@ -4,33 +4,33 @@ import java.util.Scanner
 
 // Вариант 5
 
-// Найти количество делителей числа, не делящихся на 3.
-
-tailrec fun countDivs(n: Int, predicate: (Int) -> Boolean, curDiv: Int = 1, curCount: Int = 0): Int =
-    if (curDiv > n / 2) curCount + (if (predicate(n)) 1 else 0)
-    else countDivs(n, predicate, curDiv + 1, curCount + (if (isDiv(n, curDiv) && predicate(curDiv)) 1 else 0))
-
 fun isDiv(n: Int, div: Int) = n % div == 0
+
+fun traverseDivs(n: Int, operation: (Int, Int) -> Int, predicate: (Int, Int) -> Boolean, accumInitial: Int = 0) =
+    traverseDivs(n, operation, predicate, accumInitial, 1)
+
+tailrec fun traverseDivs(n: Int, operation: (Int, Int) -> Int, predicate: (Int, Int) -> Boolean, accum: Int, curDiv: Int): Int =
+    if (curDiv > n / 2) (if (predicate(n, n)) operation(n, accum) else accum)
+    else traverseDivs(n, operation, predicate,
+        accum=(if (isDiv(n, curDiv) && predicate(curDiv, n)) operation(curDiv, accum) else accum), curDiv + 1)
+
+// Найти количество делителей числа, не делящихся на 3.
+fun countOperation(curDiv: Int, accum: Int) = accum + 1
+fun divNotDivisibleBy3(div: Int, n: Int) = div % 3 != 0
 
 // Найти сумму всех делителей числа, взаимно простых с суммой цифр числа
 // и не взаимно простых с произведением цифр числа.
-
-tailrec fun sumDivs(n: Int, predicate: (Int, Int) -> Boolean, curDiv: Int = 1, curSum: Int = 0): Int =
-    if (curDiv > n / 2) curSum + (if (predicate(n, n)) n else 0)
-    else sumDivs(n, predicate, curDiv + 1, curSum + (if (isDiv(n, curDiv) && predicate(n, curDiv)) curDiv else 0))
-
+fun sumOperation(curDiv: Int, accum: Int) = accum + curDiv
 fun areCoprime(n1: Int, n2: Int) = gcd(n1, n2) == 1
-
-fun sumDivsPredicate(n: Int, div: Int) =
-    areCoprime(div, numberTraversal(n, { a, b -> a + b })) &&
-            !areCoprime(div, numberTraversal(n, { a, b -> a * b }, 1))
+fun coprimeWithDigitSumAndNotCoprimeWithDigitProduct(div: Int, n: Int,) =
+    areCoprime(div, numberTraversal(n, { a, b -> a + b })) && !areCoprime(div, numberTraversal(n, { a, b -> a * b }, 1))
 
 fun main() {
     print("Введите натуральное число: ")
     val input = Scanner(System.`in`)
     val n = input.nextInt()
     print("Количество делителей числа, не делящихся на 3: ")
-    println(countDivs(n, {div -> div % 3 != 0}))
+    println(traverseDivs(n, ::countOperation, ::divNotDivisibleBy3))
     print("Сумма всех делителей числа, взаимно простых с суммой цифр числа и не взаимно простых с произведением цифр числа: ")
-    println(sumDivs(n, ::sumDivsPredicate))
+    println(traverseDivs(n, ::sumOperation, ::coprimeWithDigitSumAndNotCoprimeWithDigitProduct))
 }
--- ЛР5/Lab_5/src/test/Task7Test.kt
+++ ЛР5/Lab_5/src/test/Task7Test.kt
@@ -10,12 +10,12 @@ internal class Task7Test {
     @Test
     fun countDivsNotDivisibleBy3Test() {
         val expected = mapOf(1 to 1, 3 to 1, 7 to 2, 12 to 3, 16 to 5, 30 to 4, 33 to 2, 50 to 6, 100 to 9)
-        expected.forEach { assertEquals(it.value, countDivs(it.key, {div -> div % 3 != 0})) }
+        expected.forEach { assertEquals(it.value, traverseDivs(it.key, ::countOperation, ::divNotDivisibleBy3)) }
     }
 
     @Test
     fun sumDivsCoprimeWithDigitSumAndNotCoprimeWithDigitProductTest() {
         val expected = mapOf(1 to 0, 3 to 0, 7 to 0, 12 to 6, 16 to 30, 30 to 17, 33 to 0, 50 to 2, 100 to 216)
-        expected.forEach { assertEquals(it.value, sumDivs(it.key, ::sumDivsPredicate)) }
+        expected.forEach { assertEquals(it.value, traverseDivs(it.key, ::sumOperation, ::coprimeWithDigitSumAndNotCoprimeWithDigitProduct)) }
     }
 }
